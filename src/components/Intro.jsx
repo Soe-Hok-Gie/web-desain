cdf4cd27ac4bc243535f4a28613bebd87a42080f
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-28 16:48:56 +0700

    membagi batas pada halaman componenst Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    
+    height: calc(100vh - 50px);
+    display: flex;
 `;
+
+const Left = styled.div`
+    width: 60%;
+`;
+
+const Right = styled.div`
+    width: 40%;
+`;
+
 
 const Intro = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+           <Left>L</Left>
+           <Right>R</Right> 
+        </Container>
     );
 }
 
