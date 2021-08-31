b3af4a98a3e5b70c1da6b4378dffa14f752c39e1
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-31 15:34:27 +0700

    membuat container di halaman features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+`
 
 const Features = () => {
     return (
-        <div>
+        <Container>
             
-        </div>
+        </Container>
     );
 }
 
