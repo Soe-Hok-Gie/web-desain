b1a0dcd320bba0beee649250957537ea1cc35be2
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-31 15:37:12 +0700

    membagi container antara bagian left dan right di halaman features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,12 +3,19 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     display: flex;
-`
+`;
+const Left = styled.div`
+    width: 50%;
+`;
+const Right = styled.div`
+    width: 50%;
+`;
 
 const Features = () => {
     return (
         <Container>
-            
+            <Left></Left>
+            <Right></Right>
         </Container>
     );
 }
