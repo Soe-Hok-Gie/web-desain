19a9481cf6d8d0f165e6de473f98fbbacb7196f2
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-09-02 15:39:32 +0700

    membuat components wrapper

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import How from "../img/how.png";
+import How from "../img/grandma.png";
 
 const Container = styled.div`
     display: flex;
@@ -11,18 +11,39 @@ const Left = styled.div`
     width: 50%;
 `;
 const Image = styled.img`
-
+    height: 100%;
+    margin-left: 100px;
 `;
 
 const Right = styled.div`
     width: 50%;
 `;
 
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const Title = styled.h1`
+
+`;
+const Desc = styled.p`
+    font-size: 20px;
+    color: gray;
+    margin-top: 20px;
+`;
+
 const Service = () => {
     return (
         <Container>
             <Left><Image src={How}></Image></Left>
-            <Right></Right>
+            <Right>
+                <Wrapper>
+                    <Title>Trust and enjoy</Title>
+                    <Desc>We will help you with the difficulties you experience during the learning process in the field of design, so that your high target in the digital field is immediately achieved.</Desc>
+                </Wrapper>
+            </Right>
         </Container>
     );
 }
