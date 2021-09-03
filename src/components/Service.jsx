c2892ea10942cfd0a2e9c9573c6b8b4961b490ca
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-09-03 16:05:34 +0700

    membuat components Button

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -12,13 +12,15 @@ const Left = styled.div`
     width: 50%;
 `;
 const Image = styled.img`
-    height: 100%;
+    height: 75%;
     margin-left: 100px;
 `;
 
 const Right = styled.div`
     width: 50%;
 `;
+
+
 
 const Wrapper = styled.div`
     padding: 50px;
@@ -36,8 +38,22 @@ const Desc = styled.p`
 `;
 
 const CardContainer = styled.div`
-
+    display: flex;
+    justify-content: space-between;
+    margin-top: 50px;
 `;
+
+const Button = styled.button`
+    width: 180px;
+    border: none;
+    border-radius: 10px;
+    background-color: brown;
+    color: whitesmoke;
+    font-size: 20px;
+    padding: 15px;
+    margin-top: 50px;
+`;
+
 
 const Service = () => {
     return (
@@ -47,8 +63,13 @@ const Service = () => {
                 <Wrapper>
                     <Title>Trust and enjoy</Title>
                     <Desc>We will help you with the difficulties you experience during the learning process in the field of design, so that your high target in the digital field is immediately achieved.</Desc>
+                    <CardContainer>
+                        <MiniCard/>
+                        <MiniCard/>
+                        <MiniCard/>
+                    </CardContainer>
+                    <Button>Look GrandMa</Button>
                 </Wrapper>
-                <CardContainer><MiniCard/></CardContainer>
             </Right>
         </Container>
     );
