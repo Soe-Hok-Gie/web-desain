2025204ace99e438c78a5ed07b3d24326eb933a8
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-09-03 17:16:13 +0700

    membuat hook state pada components video

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -12,6 +12,7 @@ const Container = styled.div`
 
 const Left = styled.div`
     width: 50%;
+    position: relative;//berkaitan dgn posisi absolute pada components vidio
 `;
 const Image = styled.img`
     /*menangkap props image */
@@ -20,11 +21,19 @@ const Image = styled.img`
     margin-left: 80px;
 `;
 
+const Video = styled.video`
+    display: ${(props) => !props.open && "none"};
+    height: 300px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+`;
+
 const Right = styled.div`
     width: 50%;
 `;
-
-
 
 const Wrapper = styled.div`
     padding: 50px;
@@ -59,6 +68,7 @@ const Button = styled.button`
     cursor: pointer;
     display: flex;
     align-items: center;
+    outline: none;
 `;
 
 const Icon = styled.img `
@@ -71,8 +81,15 @@ const Service = () => {
     const [open, setOpen] = useState(false);
     return (
         <Container>
-            <Left><Image open={open} src={How} />
-             </Left> {/*membuat props image */}
+            <Left>
+                <Image open={open} src={How} />
+                <Video
+                    open={open}
+                    autoPlay
+                    loop
+                    controls
+                    src="https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761" />
+             </Left> {/*membuat props image dan video*/}
             <Right>
                 <Wrapper>
                     <Title> Enjoy it</Title>
