b139b0f71155d4cbb74df1f88a7430544866c18a
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-09-05 15:50:20 +0700

    membuat file Price.JSX

---
+++ src/components/Price.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100%;
+    display: flex;
+    justify-content: center;
+`
+
+const Price = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default Price;
