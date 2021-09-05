7c4dfbb58f0c757c9f39c231889888487b8c420c
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-09-05 16:40:04 +0700

    membuat PriceComponent,Price,Type,List,ListItem

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+    padding: 20px;
+    -webkit-box-shadow: 0px 0px 17px -11px rgba(0,0,0,0.58); 
+    box-shadow: 0px 0px 17px -11px rgba(0,0,0,0.58);
+    background-color: whitesmoke;
+    display: flex;
+    align-items: center;
 
 `
 const PriceContainer = styled.div``
-const Price = styled.div``
-const Type = styled.div``
-const List = styled.div``
-const ListItem = styled.div``
+const Price = styled.span``
+const Type = styled.button``
+const List = styled.ul``
+const ListItem = styled.li``
 
 
 
@@ -17,10 +23,8 @@ const PriceCard = () => {
     return (
         <Container>
             <PriceContainer>
-                $<Price>20</Price>
-
-                    <Type>  Plan </Type>
-
+                $<Price>20</Price>/month
+                    <Type>  Bassic Plan </Type>
                         <List>
                             <ListItem>A</ListItem>
                             <ListItem>CD</ListItem>
